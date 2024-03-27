6c8cbb45c1450cada008cc662a2b46ea53b7b672
pragyat1295 <56128902+pragyat1295@users.noreply.github.com>
2024-03-27 10:52:05 +0530

    Create BCGX_round1.js

---
+++ interview/BCGX_round1.js
@@ -0,0 +1,27 @@
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(function() {
+//     console.log(i);
+//   }, 0);
+// }
+
+// console.log(3 > 2 > 1);
+
+// console.log(0.1 + 0.2 == 0.3);
+// console.log(1 +  "2" + "2");
+// console.log(1 +  +"2" + "2");
+
+// export default function ParentComponent(props) {
+//   function InnerComponent() {
+//     // ...
+//   }
+
+//   return (
+//     <>
+//       <InnerComponent />
+//     </>
+//   );
+// }
+
+// localStorage.setItem('a', 8)
+// localStorage.getItem('a') === 8 
+// it will return false because returned a value would be in string
